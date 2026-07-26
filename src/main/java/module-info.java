01c9ed639b184ee36d89e8fe2ae0073ed7a02138
vinuth20241620 <vinuth.20241620@iit.ac.lk>
2026-07-27 05:01:40 +0530

    Initial JavaFX Maven project setup

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module lk.vinuth.malabesparepartssystem {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens lk.vinuth.malabesparepartssystem to javafx.fxml;
+    exports lk.vinuth.malabesparepartssystem;
+}
